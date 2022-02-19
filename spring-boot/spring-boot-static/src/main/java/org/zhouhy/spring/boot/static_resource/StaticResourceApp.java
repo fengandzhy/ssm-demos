154e98bb6d31aa88c69d246bec331c47e2dd36e5
fengandzhy <fengandzhy@gmail.com>
2022-02-19 16:21:59 +1300

    spring boot static

---
+++ spring-boot/spring-boot-static/src/main/java/org/zhouhy/spring/boot/static_resource/StaticResourceApp.java
@@ -0,0 +1,11 @@
+package org.zhouhy.spring.boot.static_resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticResourceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(StaticResourceApp.class,args);
+    }
+}
