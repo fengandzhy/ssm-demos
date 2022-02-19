32f9c044cc4c3f04e21991975c9be57bc854d48f
fengandzhy <fengandzhy@gmail.com>
2022-02-19 23:19:13 +1300

    update the usermapper

--- online-education/src/main/java/org/zhouhy/online/education/mapper/UserMapper.java
+++ online-education/src/main/java/org/zhouhy/online/education/mapper/UserMapper.java
@@ -5,9 +5,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class UserMapper {
-    private static final Map<Long, User> userMap = new HashMap<>();
+    private static final Map<String, User> userMap = new HashMap<>();
     static {
-        userMap.put(1L,new User(1L,"张三","123"));
-        userMap.put(2L,new User(2L,"李四","456"));
+        userMap.put("张三",new User(1L,"张三","123"));
+        userMap.put("李四",new User(2L,"李四","456"));
     }
 }
