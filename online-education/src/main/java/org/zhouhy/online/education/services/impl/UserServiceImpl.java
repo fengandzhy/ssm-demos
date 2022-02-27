6b6e5367cc68e3d2b4342baf364a20383c6fc48e
fengandzhy <fengandzhy@gmail.com>
2022-02-27 20:01:05 +1300

    ssm-demos online education

--- online-education/src/main/java/org/zhouhy/online/education/services/impl/UserServiceImpl.java
+++ online-education/src/main/java/org/zhouhy/online/education/services/impl/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package org.zhouhy.online.education.services.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.zhouhy.online.education.domains.User;
@@ -14,16 +16,20 @@ import java.util.UUID;
 @Service
 public class UserServiceImpl implements UserService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserServiceImpl.class);
 
     private static Map<String,User> sessionMap = new HashMap<>();
 
 
-    @Autowired
-    private UserMapper userMapper;
+    private final UserMapper userMapper;
+
+    public UserServiceImpl(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
 
     @Override
     public String login(String username, String pwd) {
-
+        LOGGER.info("In the login method!");
         User user = userMapper.login(username,pwd);
 
         if(user==null){
